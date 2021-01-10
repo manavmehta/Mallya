58e44a8ba5c09fe743462614fa299c7708048c7b
Kshitiz Jain <cauldronpumpkin@gmail.com>
2021-01-10 16:01:01 +0530

    Added NLP model and sheets API

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Mallya
 A ChatBot to attend to freshers' queries
 
+# Setup for Development
+* Add service_account.json to the following path.
+(Linux/Mac: "~/.config/gspread/service_account.json")
+(Windows: "%APPDATA%\gspread\service_account.json")
+
 # Instructions
 * Add Mallya to your Telegram by clicking [this link](https://t.me/MallyaBot)
-* Now run the following command
+* Now run the following commands
 ```py
+pip3 install -r requirements.txt
 python3 app.py
 ```
 * Play with Mallya
--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import requests as requests
 import random
 from db import db
 from config import TOKEN, BASE_TELEGRAM_URL
+import gspread
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
 
 def getChatID(update):
     return update['message']['chat']['id']
@@ -26,17 +30,54 @@ def prepareReply(db:dict, incoming_message:str, update):
     else:
         return db[incoming_message].format('')
 
+def find_answer(questions, features, answers):       # check database questions for similarity and return suitable answer.
+    corr = np.inner(features, features)
+    
+    if max(corr[0][1:]) < 0.3:
+        return "This question hasn't yet been answered."
+    else:
+        idx = corr[0][1:].argmax()
+        return questions[1:][idx]                   # change this to answers[idx] once answers have been uploaded to sheets.
+
 def index():
+
+    gc = gspread.service_account()
+
+    print ('\nLoading NLP Model...\n')
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder/4" #@param ["https://tfhub.dev/google/universal-sentence-encoder/4", "https://tfhub.dev/google/universal-sentence-encoder-large/5"]
+    model = hub.load(module_url)
+    print ('\nNLP model loaded successfully !\n')
+
+    print('\nFetching Questions...\n')
+    sht = gc.open_by_url('https://docs.google.com/spreadsheets/d/1Jged2Bis3KymVCfBqdaSUf6mJZm0iJDWai3QxtrkLHQ/edit#gid=0')
+    worksht = sht.get_worksheet(0)
+    questions = [w.lower() for w in worksht.col_values(1)[1:]]
+    answers = worksht.col_values(2)[1:]
+    print('\nQuestions fetched succesfully !\n')
+
     updateCount = getLastUpdate(BASE_TELEGRAM_URL)['update_id']
+
+    print("\nListening to Questions now !\n")
     # Keep Listening to incoming requests
     while True:
+
+        if (updateCount + 1) / 100 != updateCount / 100:                #fetching updates(if any) in sheet
+            questions = [w.lower() for w in worksht.col_values(1)[1:]]
+            answers = worksht.col_values(2)[1:]
+
         update = getLastUpdate(BASE_TELEGRAM_URL)
         if updateCount == update['update_id']:
+            
             incoming_message = getMessageText(update).lower()
-            if incoming_message in db:
-                send_message(getChatID(update), prepareReply(db, incoming_message, update))
-            else:
-                send_message(getChatID(update), 'Hindi bol bsdk')
+            msg_embeddings = model([incoming_message] + questions)
+            
+
+            send_message(getChatID(update), find_answer([incoming_message] + questions, msg_embeddings, answers))
+            
+            # if incoming_message in db:
+            #     send_message(getChatID(update), prepareReply(db, incoming_message, update))
+            # else:
+            #     send_message(getChatID(update), 'Hindi bol bsdk')
             updateCount += 1
 
 if __name__ == '__main__':
