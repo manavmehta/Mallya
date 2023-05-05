397eec25a84c6eb6633c191da4bf08db4a71a227
Manav Mehta <manavm@lambdatest.com>
2023-05-05 19:30:01 +0530

    archived old code everything works :)

---
+++ archived/commands.py
@@ -0,0 +1,7 @@
+commands = {
+    "start": "Hello there {}! I am a bot sent by the heavens to be yours. Type /help and know what I can do for you.",
+    "hi": "Hello there {}!",
+    "hello": "Hello there {}!",
+    "hey": "Hello there {}!",
+    "help": "Hey, {}. I see you sought my help.\n\nI know the following commands:\n/Hi, /Hey, /Hello - They're pretty much the same😬\n/Help - To know how I can help you.\n\n/q followed by your query - regarding IIT Mandi. \ne.g:\n/q What all facilities are available in campus?\n\nFor General questions you can just ask straight-away.\ne.g:\nWhat is your name\nTell me a joke\n\nYou can also use /u, /d and /n to upvote, downvote and get the next answer respectively. These three are only valid when you ask a query.",
+}
